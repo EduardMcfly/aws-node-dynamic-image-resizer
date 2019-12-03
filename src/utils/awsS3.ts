20a47cabe8a409aefd668968826320da35bafcce
andrescastellanos112013 <andrescastellanos112013@gmail.com>
2019-12-03 11:02:23 -0500

    awsS3 now works in offline mode

--- src/utils/awsS3.ts
+++ src/utils/awsS3.ts
@@ -1,4 +1,16 @@
 import AWS from 'aws-sdk';
-const { BUCKET = 'cosva' } = process.env;
+const {
+  BUCKET = 'cosva',
+  IS_OFFLINE,
+  ACCESS_KEY_ID,
+  SECRET_ACCESS_KEY,
+} = process.env;
+
 export { BUCKET };
-export const awsS3 = new AWS.S3();
+export const awsS3 = new AWS.S3(
+  (IS_OFFLINE && {
+    accessKeyId: ACCESS_KEY_ID,
+    secretAccessKey: SECRET_ACCESS_KEY,
+  }) ||
+    undefined,
+);
